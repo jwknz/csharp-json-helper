98ec459312052d7f77697b320818ba49c6a02755
Jeff Kranenburg <jeff.kranenburg@gmail.com>
2019-02-01 19:53:51 +1300

    Initial Commit

---
+++ JSON-Display-Dictionary/JKJson.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JKJsonDecode
+{
+    class JKJson
+    {
+        public static List<dynamic> ListOut(object des)
+        {
+            return JArray.FromObject(des).ToObject<List<dynamic>>();
+        }
+
+        public static Dictionary<string, dynamic> DictOut(object des)
+        {
+            return JObject.FromObject(des).ToObject<Dictionary<string, dynamic>>();
+        }
+
+        public static object Deserialized(string url)
+        {
+            using (WebClient w = new WebClient())
+            {
+                object d = null;
+
+                try
+                {
+                    string json_data = w.DownloadString(url);
+                    dynamic json = JsonConvert.DeserializeObject<dynamic>(json_data);
+
+                    switch (json.GetType().ToString())
+                    {
+                        case "Newtonsoft.Json.Linq.JObject":
+                            d = JObject.FromObject(json);
+                            break;
+                        case "Newtonsoft.Json.Linq.JArray":
+                            d = JArray.FromObject(json);
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    return e.Message;
+                }
+
+                return d;
+            }
+        }
+    }
+}
---
+++ JSON-Display-Dictionary/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using JKJsonDecode;
+
+namespace JSON_Display_Dictionary
+{
+    class Program
+    {
+        public static string[] Links = new string[]
+        {
+            "http://uinames.com/api/?amount=3", //Simple List
+            "https://reqres.in/api/users?page=2", //Dictionary
+            "https://jsonplaceholder.typicode.com/users" //Complex List
+        };
+
+        static void Main(string[] args)
+        {
+            DemoList(Links[0]);
+            DemoDict(Links[1]);
+            DemoList(Links[2]);
+        }
+
+        public static void DemoDict(string url)
+        {
+            //Dictionary
+
+            //View raw JSON
+            Console.WriteLine(JKJson.Deserialized(url));
+
+            //Its a dictionary, you can loop through this now and find keys and values
+            Console.WriteLine(JKJson.DictOut(JKJson.Deserialized(url)));
+
+            //See...
+            //First check what property holds the array - then treat it like a list, because it is one
+            //Access the properties, by accessing the index of the list and then the property
+            Console.WriteLine(JKJson.DictOut(JKJson.Deserialized(url))["data"][0]["first_name"]);
+        }
+
+        public static void DemoList(string url)
+        {
+            //List
+
+            //View raw JSON
+            Console.WriteLine(JKJson.Deserialized(url));
+
+            //Its a list, you can loop through this now
+            Console.WriteLine(JKJson.ListOut(JKJson.Deserialized(url)));
+
+            //See...
+            //Access the properties, by accessing the index of the list and then the property
+            Console.WriteLine(JKJson.ListOut(JKJson.Deserialized(url))[0]["name"]);
+        }
+    }
+}
